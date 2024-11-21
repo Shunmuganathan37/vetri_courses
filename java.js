c4bb5b36a0991dd735b320525f94065f504c3f0d
Shunmuganathan <shunmuganathanlakshmanan@gmail.com>
2024-11-21 16:49:58 +0530

    sathish

--- java.js
+++ java.js
@@ -35,3 +35,40 @@
   const homePage = document.getElementById('Home');
   const frontPage = document.getElementById('Front');
 
+//btn
+
+const body = document.body;
+const btn = document.querySelectorAll('.button')[0];
+
+btn.addEventListener('mouseenter', () => {
+	body.classList.add('hover');
+});
+
+btn.addEventListener('mouseleave', () => {
+	body.classList.remove('hover');
+});
+
+
+// trending - courses
+$(document).ready(function(){
+  $('.customer-logos').slick({
+      slidesToShow: 6,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 1500,
+      arrows: false,
+      dots: false,
+      pauseOnHover: false,
+      responsive: [{
+          breakpoint: 768,
+          settings: {
+              slidesToShow: 4
+          }
+      }, {
+          breakpoint: 520,
+          settings: {
+              slidesToShow: 3
+          }
+      }]
+  });
+});
